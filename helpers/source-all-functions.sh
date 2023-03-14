9a8923a389a5def0d3b0090246955ed4a0210967
Eduardo Sanchez <eduardo.sanchez@accedo.tv>
2023-03-14 11:40:19 -0400

    refactor: Add comments in source functions script

--- helpers/source-all-functions.sh
+++ helpers/source-all-functions.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
+# This script is meant to be sourced
+# from the root of the repository
+# It will source all the functions to the current session
+
 for FILE in examples/fzf/**/function-*; do
-  # shellcheck source=/dev/null # to ignore the  errorbash-language-server
+  # shellcheck source=/dev/null
   source "$FILE"
 done
